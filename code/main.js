d58caba0d3708b6e617414d66163c95c9b169936
Lê Thanh Bình <51404183+sieuquayskt3@users.noreply.github.com>
2020-12-05 00:24:10 +0700

    update

--- code/main.js
+++ code/main.js
@@ -1,1 +1,8 @@
-// shopping cart
+// shopping cart and voucher detail
+function updatePrice(id){
+    console.log("updatePrice run");
+}
+// custom voucher
+function createVoucher(){
+    console.log("createVoucher run");
+}
