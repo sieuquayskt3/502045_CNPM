6a87f885c7546aac89a94586e0b60c578c711e22
Lê Thanh Bình <51404183+sieuquayskt3@users.noreply.github.com>
2020-12-05 01:13:04 +0700

    update

--- code/main.js
+++ code/main.js
@@ -1,6 +1,30 @@
-// shopping cart and voucher detail
-function updatePrice(id){
-    console.log("updatePrice run");
+// shopping cart
+function convertPrice(n){
+    //TODO tiếp tục
+    if(n<100000){
+        return n;
+    }
+    let result = '';
+    let num = n.toString();
+    console.log(num[-3]);
+    while (n > 9999) {
+        result += '.'+(n%10000); // 101854221 => .221 => .854.221
+        //console.log(result);
+
+        n = Math.floor(n/10000);
+    }
+    result = n+result;    // .854.221 => 101.854.221
+    return result+'đ';
+}
+function changePrice(o){
+    let inputID = o.id;
+    let priceID = 'price'+inputID;
+    let value = $('#'+inputID).val();
+    let result = value*300000;
+    $('#'+priceID).html(result);
+    // cập nhật tổng số tiền
+    // let totalPrice = value + $('#'+totalPrice).html
+    // $('#'+totalPrice).html(result);
 }
 // custom voucher
 function createVoucher(){
