eab646d4f4a2357f2ccb1575a1d7b1867d74cf14
Lê Thanh Bình <51404183+sieuquayskt3@users.noreply.github.com>
2020-12-05 11:00:05 +0700

    update main.js
    có thể tạo code bằng số nhập trong input

--- code/main.js
+++ code/main.js
@@ -11,7 +11,7 @@ function changePrice(o){
     let inputID = o.id;
     let priceID = 'price'+inputID;
     let value = $('#'+inputID).val();
-    
+
     let result = value*300000;
     let vnd = convertPrice(result);
     $('#'+priceID).html(vnd);
@@ -20,6 +20,23 @@ function changePrice(o){
     // $('#'+totalPrice).html(result);
 }
 // custom voucher
+function generateVoucher(n){
+    var result           = '';
+    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < n; i++ ) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+}
+
 function createVoucher(){
-    console.log("createVoucher run");
+    let number = $('#number').val();
+    for (let i = 0; i<number; i++){
+        let container = document.getElementById("voucherList");
+        let node = document.createElement("li");
+        let voucher = document.createTextNode(generateVoucher(8));
+        node.appendChild(voucher);
+        container.appendChild(node);
+    }
 }
